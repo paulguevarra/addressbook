9f228ec211369fc62058c82f24b5b68cc36fb3c3
Paul Guevarra <p.a.guevarra@gmail.com>
2017-07-16 16:08:34 -0700

    create a fullName method on the contact prototype so we can eaily see the full name of each person in address book

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,9 @@ function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
 }
-
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
 //user interface logic
 $(document).ready(function(){
   $("form#new-contact").submit(function(event){
@@ -14,7 +16,7 @@ $(document).ready(function(){
 
     var newContact = new Contact (inputtedFirstName, inputtedLastName);
 
-    $("ul#contacts").append("<li><span class='contact'>" +newContact.firstName + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $(".contact").last().click(function() {                 /* last() binds the event to the most recently-inserted contact*/
       $("#show-contact").show();
